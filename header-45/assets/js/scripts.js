cbf9a7a59abf90f671cfa1b260edb8454bcb1b9b
jahid284 <amijahid284@gmail.com>
2015-08-10 16:42:47 +0600

    commit for all headers

---
+++ header-45/assets/js/scripts.js
@@ -0,0 +1,83 @@
+jQuery(function ($) {
+
+    'use strict';
+
+
+    // -------------------------------------------------------------
+    // Preloader
+    // -------------------------------------------------------------
+    (function () {
+        $('#status').fadeOut();
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+
+
+
+
+    // -------------------------------------------------------------
+    // Sticky Menu
+    // -------------------------------------------------------------
+
+    (function () {
+        var nav = $('.navbar');
+        var scrolled = false;
+
+        $(window).scroll(function () {
+
+            if (110 < $(window).scrollTop() && !scrolled) {
+                nav.addClass('sticky animated fadeInDown').animate({ 'margin-top': '0px' });
+
+                scrolled = true;
+            }
+
+            if (110 > $(window).scrollTop() && scrolled) {
+                nav.removeClass('sticky animated fadeInDown').css('margin-top', '0px');
+
+                scrolled = false;
+            }
+        });
+
+    }());
+
+
+
+
+
+    // -------------------------------------------------------------
+    // OffCanvas
+    // -------------------------------------------------------------
+    (function () {
+        $('button.navbar-toggle').HippoOffCanvasMenu({
+        documentWrapper: '#main-wrapper',
+        contentWrapper : '.content-wrapper',
+        position       : 'hippo-offcanvas-left',    // class name
+        // opener         : 'st-menu-open',            // class name
+        effect         : 'slide-in-on-top',             // class name
+        closeButton    : '#off-canvas-close-btn',
+        menuWrapper    : '.offcanvas-menu',                 // class name below-pusher
+        documentPusher : '.offcanvas-pusher'
+        });
+    }());
+
+
+
+    // -------------------------------------------------------------
+    // dropdown hover
+    // -------------------------------------------------------------
+    (function () {
+         $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');            
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');              
+            }
+        );
+    }());
+
+
+
+
+}); // JQuery end
